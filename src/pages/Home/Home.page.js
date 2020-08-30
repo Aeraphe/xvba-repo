d5f307d3c960dee7942cf3a1fa1aa90383f3cd85
aeraph <alberto.aeraph@gmail.com>
2020-08-30 18:41:47 -0300

    Create Home Page

---
+++ src/pages/Home/Home.page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { SearchComp } from './components/Search.comp';
+
+
+
+export const HomePage = ()=>{
+
+    return(
+        <div>
+            <SearchComp></SearchComp>
+        </div>
+    )
+}
