20a1bbfae78aba89955385c54b15132dd05a0b14
aeraph <alberto.aeraph@gmail.com>
2020-08-30 20:29:39 -0300

    Create SerachResultBarComp

---
+++ src/pages/SearchResult/components/SearchResultBar/SearchResultBar.comp.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './SearchResultBarComp.module.css';
+
+
+export const SerachResultBarComp = () => {
+
+    return (
+        <div className={styles.SearchResultBar}>
+            <div>
+                Find 10 results
+            </div>
+            <div>
+                1,2,3,4...
+            </div>
+        </div>
+    )
+}
