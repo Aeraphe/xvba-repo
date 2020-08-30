dd255b1dacfd0d5ba0b4e285bb7a617f1aca060d
aeraph <alberto.aeraph@gmail.com>
2020-08-30 18:42:30 -0300

    Create HeaderComp for template

---
+++ src/template/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './Header.module.css'
+
+
+
+export const Header = () => {
+
+    return (
+        <header className={styles.header}>
+            <a href="/"><span style={{color:'#81B441', fontWeight:'bold'}}>X</span>vba<span style={{color:'#6B913A'}}>repository</span></a>
+            <nav >
+                <ul className={styles['header-menu']}>
+                    <li><a href="/">Login</a></li>
+                    <li><a href="/">Login</a></li>
+                </ul>
+            </nav>
+        </header>
+
+    )
+}
