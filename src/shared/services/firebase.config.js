f76e7a1f9c303acea27bbabc2286c7583124d005
aeraph <alberto.aeraph@gmail.com>
2020-08-30 15:01:36 -0300

    First commit

---
+++ src/shared/services/firebase.config.js
@@ -0,0 +1,15 @@
+
+
+// TODO: Replace the following with your app's Firebase project configuration
+export const firebaseConfig = {
+    apiKey: "AIzaSyDfj-YPcb9GHW_3I9_ScGlTtY4jMSGqU1U",
+    authDomain: "xvba-691e3.firebaseapp.com",
+    databaseURL: "https://xvba-691e3.firebaseio.com",
+    projectId: "xvba-691e3",
+    storageBucket: "xvba-691e3.appspot.com",
+    messagingSenderId: "302539319269",
+    appId: "1:302539319269:web:796eca802d390e732c710c",
+    measurementId: "G-XMC50PMV75"
+  };
+  
+ 
