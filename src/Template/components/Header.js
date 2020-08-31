6fc13a336fd5ce8616c071d11ded768ee52314e3
aeraph <alberto.aeraph@gmail.com>
2020-08-31 15:43:43 -0300

    Add new links to header component

--- src/Template/components/Header.js
+++ src/Template/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './Header.module.css'
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 
 
@@ -8,12 +8,13 @@ export const Header = () => {
 
     return (
         <header className={styles.header}>
-            <a href="/"><span style={{color:'#81B441', fontWeight:'bold'}}>X</span>vba<span style={{color:'#6B913A'}}>repository</span></a>
+            <a href="/"><span style={{ color: '#81B441', fontWeight: 'bold' }}>X</span>vba<span style={{ color: '#6B913A' }}>repository</span></a>
             <nav >
                 <ul className={styles['header-menu']}>
-                    <li><a href="/">Suport</a></li>
-                    <li><a href="/">Tutorial</a></li>
-                    <li><a href="/">Login</a></li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/suport">Suport</Link></li>
+                    <li><Link to="/tutorial">Tutorial</Link></li>
+                    <li><Link to="login">Login</Link></li>
                 </ul>
             </nav>
         </header>
