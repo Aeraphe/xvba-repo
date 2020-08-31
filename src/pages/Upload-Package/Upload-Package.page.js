b041b3d6c36985dd2a755c8de5742fc5250e4206
aeraph <alberto.aeraph@gmail.com>
2020-08-31 18:15:18 -0300

    Change logo size

--- src/pages/Upload-Package/Upload-Package.page.js
+++ src/pages/Upload-Package/Upload-Package.page.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { XvbaLogoSharedComp } from '../../shared/components/Xvba-Logo.shared.component'
-import { UploadPackageForm } from "./components/Upload-Package-Form.comp";
+import { UploadPackageForm } from "./components/Upload-Form/Upload-Package-Form.comp";
 import styles from "./Upload-Package.module.css";
 
 export const UploadPackagePage = () => {
@@ -8,7 +8,7 @@ export const UploadPackagePage = () => {
     return (
         <div className={styles['Upload-Package']}>
             <div>
-                <XvbaLogoSharedComp size="5rem">
+                <XvbaLogoSharedComp size="7rem">
                     <p><b>Publish new package</b></p>
                 </XvbaLogoSharedComp>
                 <UploadPackageForm></UploadPackageForm>
