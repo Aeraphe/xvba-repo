95bd242b2db100a971f6f597f80eb702b87dc3d6
aeraph <alberto.aeraph@gmail.com>
2020-08-31 21:59:09 -0300

    Add login functions

--- src/pages/Login/components/Login-Form.component.js
+++ src/pages/Login/components/Login-Form.component.js
@@ -1,34 +1,59 @@
-import React from 'react';
-//import { firebaseLoginService } from "../../../shared/services/loggin.service";
+import React, { useState } from 'react';
+import FirebaseService from "../../../shared/services/loggin.service";
 import styles from "./Login-Form.module.css";
-import {CardShared} from '../../../shared/components/Card/Card.shared'
+import { CardShared } from '../../../shared/components/Card/Card.shared'
 
 export const LoginFormComponent = (props) => {
-    let loggin = false;
-
+    const [loginStatus, setLoginStatus] = useState(false);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
     return (
         <div>
-            <div style={{ display: !loggin ? "block" : "none" }}>
+            <div style={{ display: !loginStatus ? "block" : "none" }}>
                 <form className={styles['Login-Form']}>
                     <div className={styles['Login-Form-Content']} >
                         {props.children}
                         <p>Login</p>
-                        <label for='user-email'>e-mail: </label>
-                        <input id='user-email' type='email' ></input>
-                        <label for='user-pass'>password: </label>
-                        <input id='user-pass' type='password'></input>
-                        <button className={styles['Form-Btn']} type='submit'>Login</button>
+                        <label htmlFor='user-email'>Email Address: </label>
+                        <input id='user-email' type='email' name="email" value={email} onChange={e => setEmail(e.target.value)} ></input>
+                        <label htmlFor='user-pass'>password: </label>
+                        <input id='user-pass' type='password' name="password" value={password} onChange={e => setPassword(e.target.value)}></input>
+                        <button className={styles['Form-Btn']} type="button" onClick={e => login(e)}>Login</button>
                     </div>
                 </form>
             </div>
-            <div style={{ display: loggin ? "block" : "none" }}>
+            <div style={{ display: loginStatus ? "block" : "none" }}>
                 <CardShared height="180px" width="250px">
-                {props.children}
-                <button className={styles['Form-Logout-Btn']} type="button">Logout</button>
+                    {props.children}
+                    <button className={styles['Form-Logout-Btn']} type="submit" onClick={e => logout(e)}>Logout</button>
                 </CardShared>
-                
+
             </div>
         </div>
 
     )
+
+    async function login(e) {
+        e.preventDefault();
+        try {
+
+            await FirebaseService.login(email, password);
+            setLoginStatus(true);
+
+        } catch (error) {
+            setLoginStatus(false);
+        }
+    }
+
+    async function logout(e) {
+        e.preventDefault();
+        try {
+            await FirebaseService.logout();
+            setLoginStatus(false);
+        } catch (error) {
+            console.error(error.message);
+        }
+
+    }
+
 }
