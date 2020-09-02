a5db84a7761b8fae96da34308b6d949e3f14050d
aeraph <alberto.aeraph@gmail.com>
2020-09-02 16:52:05 -0300

    Add reducer provider component

--- src/App.js
+++ src/App.js
@@ -9,21 +9,27 @@ import { SearchPage } from './pages/Search/Search.page'
 import { LoginPage } from './pages/Login/Login.page';
 import { ShowPackagePage } from "./pages/Show-Package/Show-Package.page";
 import { UploadPackagePage } from './pages/Upload-Package/Upload-Package.page';
+import { Provider } from "react-redux";
+import  store from "./store";
 // Initialize Firebase
 fb.initializeApp(firebaseConfig);
 
+
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Template>
-          <Route path='/' exact component={SearchPage}></Route>
-          <Route path='/login' exact component={LoginPage}></Route>
-          <Route path='/show-package/:package' exact component={ShowPackagePage} ></Route>
-          <Route path='/upload-package' exact component={UploadPackagePage}></Route>
-        </Template>
-      </div>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <div className="App">
+          <Template>
+            <Route path='/' exact component={SearchPage}></Route>
+            <Route path='/login' exact component={LoginPage}></Route>
+            <Route path='/show-package/:package' exact component={ShowPackagePage} ></Route>
+            <Route path='/upload-package' exact component={UploadPackagePage}></Route>
+          </Template>
+        </div>
+      </BrowserRouter>
+    </Provider>
+
 
   );
 }
