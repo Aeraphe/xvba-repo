bf171a9cd5890ee6e206f8aa1a36c023ca272d3f
aeraph <alberto.aeraph@gmail.com>
2020-09-05 16:13:39 -0300

    Start to update upload-package-form for use Modal

---
+++ src/pages/Admin-Package/components/Upload-Form/Upload-Package-Form.comp.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from "./Upload-Package-Form.module.css";
+export const UploadPackageForm = () => {
+
+    return (
+        <div>
+            <form className={styles['Upload-Package-Form']}>
+                <div className={styles['Upload-Package-Form-Container']}>
+                    <label for="package_name"> Package name: </label>
+                    <input id="package_name" type="text" placeholder="Package Name"></input>
+                    <p><input type="file" ></input></p>
+                    <button className={styles['Form-Btn']} type="submit">Upload</button>
+                </div>
+
+            </form>
+        </div>
+    )
+
+}
