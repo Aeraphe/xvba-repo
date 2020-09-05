f73c9ddf959173c7e0e4a36cadcdeaa28b135c51
aeraph <alberto.aeraph@gmail.com>
2020-09-05 19:46:14 -0300

    Fix html

--- src/pages/Admin-Package/components/Upload-Form/Upload-Package-Form.comp.js
+++ src/pages/Admin-Package/components/Upload-Form/Upload-Package-Form.comp.js
@@ -6,7 +6,7 @@ export const UploadPackageForm = () => {
         <div>
             <form className={styles['Upload-Package-Form']}>
                 <div className={styles['Upload-Package-Form-Container']}>
-                    <label for="package_name"> Package name: </label>
+                    <label htmlFor="package_name"> Package name: </label>
                     <input id="package_name" type="text" placeholder="Package Name"></input>
                     <p><input type="file" ></input></p>
                     <button className={styles['Form-Btn']} type="submit">Upload</button>
