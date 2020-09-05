b845cb1434b07dc027342a9b9a6e77493011162a
aeraph <alberto.aeraph@gmail.com>
2020-09-05 19:45:56 -0300

    Fix modal css and show event

--- src/shared/components/XVBA-Modal/Modal.js
+++ src/shared/components/XVBA-Modal/Modal.js
@@ -1,17 +1,19 @@
-import React,{useState} from 'react'
+import React from 'react'
 import styles from './Modal.module.css'
 
 export const Modal = (props) => {
-    const [display,setDisplay] = useState(false);
+
     return (
-        <div style={{ display: display ? 'block' : 'none' }} className={styles['Modal']}>
+        <div style={{ display: props.show ? 'block' : 'none' }} className={styles['Modal-Outer']}>
+            <div onClick={props.showModal()} className={styles['Modal']}>
+            </div>
             <div style={{ height: props.height }} className={styles['Modal-Container']}>
                 <div className={styles['Modal-Content']}>
                     {props.children}
                 </div>
-             
-                <div onClick={()=>setDisplay(!display)} className={styles['Modal-Close']}>
-                    <span className={styles['Modal-Close-btn']}>X</span>
+
+                <div className={styles['Modal-Close']}>
+                    <span onClick={props.showModal()} className={styles['Modal-Close-btn']}>X</span>
                 </div>
             </div>
         </div>
