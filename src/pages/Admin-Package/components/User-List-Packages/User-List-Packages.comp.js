5c1652946f42d1ac9635400619704aa987e046c0
aeraph <alberto.aeraph@gmail.com>
2020-09-05 18:39:31 -0300

    Create List Item

--- src/pages/Admin-Package/components/User-List-Packages/User-List-Packages.comp.js
+++ src/pages/Admin-Package/components/User-List-Packages/User-List-Packages.comp.js
@@ -5,15 +5,42 @@ export const UserListPackagesComp = () => {
 
 
     return (
-        <div className={styles['Container']}>
-           <div className={styles['Header']}>
-               <div>Item</div>
-               <div>Name</div>
-               <div>Version</div>
-               <div>Stars</div>
-               <div>Dow</div>
-               <div>Options</div>
-           </div>
+        <div>
+            <p className={styles['Title']}>:: User Packages</p>
+            <div className={styles['Container']}>
+                <div className={styles['Header']}>
+                    <div>Item</div>
+                    <div>Name</div>
+                    <div>Version</div>
+                    <div>Stars</div>
+                    <div>Installs</div>
+                    <div>Options</div>
+                </div>
+
+            </div>
+
+            <div>
+                <PackageListItem></PackageListItem>
+                <PackageListItem></PackageListItem>
+            </div>
+        </div>
+
+    )
+}
+
+const PackageListItem = () => {
+    return (
+        <div className={styles['Body-Container']}>
+
+
+            <div className={styles['Body-Item']}>
+                <div>Item</div>
+                <div>Name</div>
+                <div>Version</div>
+                <div>Rating</div>
+                <div>Installs</div>
+                <div>Options</div>
+            </div>
         </div>
     )
 }
