342285ef1c4d1835c416eaee017be0430d52473b
aeraph <alberto.aeraph@gmail.com>
2020-09-05 16:10:40 -0300

    Change Page name (Update Route)

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { Route } from 'react-router-dom';
 import { SearchPage } from './pages/Search/Search.page'
 import { LoginPage } from './pages/Login/Login.page';
 import { ShowPackagePage } from "./pages/Show-Package/Show-Package.page";
-import { UploadPackagePage } from './pages/Upload-Package/Upload-Package.page';
+import { AdminPackagePage } from './pages/Admin-Package/Admin-Package.page';
 import { Provider } from "react-redux";
 import  store from "./store";
 // Initialize Firebase
@@ -24,7 +24,7 @@ function App() {
             <Route path='/' exact component={SearchPage}></Route>
             <Route path='/login' exact component={LoginPage}></Route>
             <Route path='/show-package/:package' exact component={ShowPackagePage} ></Route>
-            <Route path='/upload-package' exact component={UploadPackagePage}></Route>
+            <Route path='/upload-package' exact component={AdminPackagePage}></Route>
           </Template>
        
       </BrowserRouter>
