79aafb3f2cfb80a09fb77f8958c845be86973a19
aeraph <alberto.aeraph@gmail.com>
2020-09-06 13:12:05 -0300

    Improve Header Css

--- src/Template/components/Header.js
+++ src/Template/components/Header.js
@@ -5,20 +5,22 @@ import { Link } from 'react-router-dom';
 let isLogged;
 
 export const Header = () => {
-    isLogged = useSelector(state=>state.auth.isLogged);
+    isLogged = useSelector(state => state.auth.isLogged);
     return (
-        <header className={styles.header}>
-            <a href="/"><span style={{ color: '#81B441', fontWeight: 'bold' }}>X</span>vba<span style={{ color: '#6B913A' }}>repository</span></a>
-            <nav >
-                <ul className={styles['header-menu']}>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/donate">Donate</Link></li>
-                    <li><Link to="/tutorial">Tutorial</Link></li>
-                    <li> <Link to="/upload-package">Publish</Link></li>
-                    <li><Link to="login">{isLogged?'Logout':'Login'}</Link></li>
-                </ul>
-            </nav>
-        </header>
+        <div className={styles['Container']} >
+            <header className={styles.header}>
+                <a href="/"><span style={{ color: '#81B441', fontWeight: 'bold' }}>X</span>vba<span style={{ color: '#6B913A' }}>repository</span></a>
+                <nav >
+                    <ul className={styles['header-menu']}>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/donate">Donate</Link></li>
+                        <li><Link to="/tutorial">Tutorial</Link></li>
+                        <li> <Link to="/upload-package">Publish</Link></li>
+                        <li><Link to="login">{isLogged ? 'Logout' : 'Login'}</Link></li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
 
     )
 }
