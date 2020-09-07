5969aebeacadcf6714af9941f6f4c1c4364a7bd4
aeraph <alberto.aeraph@gmail.com>
2020-09-07 19:10:34 -0300

    Add Http request for packages

--- src/pages/Search/Search.page.js
+++ src/pages/Search/Search.page.js
@@ -1,20 +1,45 @@
-import React from 'react';
-import { SerachResultBarComp } from './components/SearchResultBar/SearchResultBar.comp'
+import React, { useState, useEffect } from 'react';
+import { SearchResultBarComp } from './components/SearchResultBar/SearchResultBar.comp'
 import { SearchResultListComp } from './components/SearchResultList/SearchResultList.comp'
 import { SearchComp } from './components/Search/Search.comp';
+import PackagesHttpService from '../../shared/services/packagesHttp.service';
+
 export const SearchPage = () => {
+
+    const [packages, setPackages] = useState([]);
+    //Get all packages from firestore
+    async function get() { setPackages(await handlerGetPackages()) }
+
+    useEffect(() => {
+        get();
+    }, []);
+
     return (
-        <div>
+        <div style={{ marginBottom: '57px' }}>
             <SearchComp></SearchComp>
-            <SerachResultBarComp></SerachResultBarComp>
-            <SearchResultListComp 
-            package="GausCurve" 
-            user="Beto" 
-            description="Fucntion for calc Gaus Cruves"
-            publishdate="14/08/1980"
-            ></SearchResultListComp>
-            <SearchResultListComp></SearchResultListComp>
-            <SearchResultListComp></SearchResultListComp>
+            <SearchResultBarComp></SearchResultBarComp>
+            {packages}
+
         </div>
     )
+}
+
+
+const handlerGetPackages = async () => {
+
+    let packages = await PackagesHttpService.get();
+    let response = [];
+
+    packages.forEach((item, index) => {
+        response.push(
+            <SearchResultListComp key={index}
+                package={item.package.name}
+                user="Beto"
+                description="Function for calc Gauss Curves"
+                publish_date="14/08/1980"
+            ></SearchResultListComp>
+        )
+    });
+    return response;
+
 }
