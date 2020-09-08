d3e7a84ca22407a9d8a0362bc21b032a7d023abb
aeraph <alberto.aeraph@gmail.com>
2020-09-08 14:46:00 -0300

    Refactory Search Component for http request

--- src/pages/Search/Search.page.js
+++ src/pages/Search/Search.page.js
@@ -1,45 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import { SearchResultBarComp } from './components/SearchResultBar/SearchResultBar.comp'
-import { SearchResultListComp } from './components/SearchResultList/SearchResultList.comp'
+import React from 'react';
 import { SearchComp } from './components/Search/Search.comp';
-import PackagesHttpService from '../../shared/services/packagesHttp.service';
+
 
 export const SearchPage = () => {
 
-    const [packages, setPackages] = useState([]);
-    //Get all packages from firestore
-    async function get() { setPackages(await handlerGetPackages()) }
 
-    useEffect(() => {
-        get();
-    }, []);
 
     return (
         <div style={{ marginBottom: '57px' }}>
             <SearchComp></SearchComp>
-            <SearchResultBarComp></SearchResultBarComp>
-            {packages}
-
         </div>
     )
 }
 
-
-const handlerGetPackages = async () => {
-
-    let packages = await PackagesHttpService.get();
-    let response = [];
-
-    packages.forEach((item, index) => {
-        response.push(
-            <SearchResultListComp key={index}
-                package={item.package.name}
-                user="Beto"
-                description="Function for calc Gauss Curves"
-                publish_date="14/08/1980"
-            ></SearchResultListComp>
-        )
-    });
-    return response;
-
-}
--- src/pages/Search/components/Search/Search.comp.js
+++ src/pages/Search/components/Search/Search.comp.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Search.module.css';
 import { XvbaLogoSharedComp } from '../../../../shared/components/Xvba-Logo.shared.component';
-
+import { SearchResultBarComp } from '../SearchResultBar/SearchResultBar.comp';
+import { SearchResultListComp } from '../SearchResultList/SearchResultList.comp';
+import PackagesHttpService from '../../../../shared/services/packagesHttp.service';
+import _ from 'lodash';
 
 export const SearchComp = () => {
+    const [packages, setPackages] = useState([]);
+    const [search, setSearch] = useState("");
+
+    const debounceSearch = _.debounce((val) => {
+        if (val) {
+            setSearch(val)
+        }
+    }, 500)
+
+    const handleOnChangeSearchText = (e) => {
+        debounceSearch(e.target.value)
+    }
+    const handleOnClickSearch = async () => {
+        setPackages(await handlerGetPackages(search))
+    }
+
 
     return (
         <div>
@@ -11,19 +30,34 @@ export const SearchComp = () => {
                 <XvbaLogoSharedComp size="7rem"></XvbaLogoSharedComp>
             </div>
             <div className={styles['Search-Container']}>
-                <SearchInputComp></SearchInputComp>
+                <input onChange={(e) => handleOnChangeSearchText(e)} placeholder="Search VBA Package" className={styles['Search-Input']}></input>
+                <button onClick={() => handleOnClickSearch()} className={styles['Search-Input-Btn']}>Search</button>
             </div>
-
+            <SearchResultBarComp></SearchResultBarComp>
+            {packages}
         </div>
 
     )
 }
 
-const SearchInputComp = () => {
-    return (
-        <div>
-            <input placeholder="Search VBA Package" className={styles['Search-Input']}></input>
-            <button className={styles['Search-Input-Btn']}>Search</button>
-        </div>
-    )
+
+
+const handlerGetPackages = async (val) => {
+
+    let packages = await PackagesHttpService.search(val);
+    let response = [];
+    if (packages?.data) {
+        packages.data.forEach((item, index) => {
+            response.push(
+                <SearchResultListComp key={index}
+                    package={item.package.name}
+                    user="Beto"
+                    description="Function for calc Gauss Curves"
+                    publish_date="14/08/1980"
+                ></SearchResultListComp>
+            )
+        });
+    }
+    return response;
+
 }
