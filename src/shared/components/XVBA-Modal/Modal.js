3b24750129f2f95ea1e42927e3920c8075bcb7a9
aeraph <alberto.aeraph@gmail.com>
2020-09-08 16:58:32 -0300

    Fix modal size

--- src/shared/components/XVBA-Modal/Modal.js
+++ src/shared/components/XVBA-Modal/Modal.js
@@ -7,7 +7,7 @@ export const Modal = (props) => {
         <div style={{ display: props.show ? 'block' : 'none' }} className={styles['Modal-Outer']}>
             <div onClick={props.showModal()} className={styles['Modal']}>
             </div>
-            <div style={{ height: props.height }} className={styles['Modal-Container']}>
+            <div  className={styles['Modal-Container']}>
                 <div className={styles['Modal-Content']}>
                     {props.children}
                 </div>
