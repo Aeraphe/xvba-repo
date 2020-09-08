7bc1f9713ec39a4a037a5e6d12b45c73bbe499e7
aeraph <alberto.aeraph@gmail.com>
2020-09-08 16:41:02 -0300

    Add description field on form

--- src/pages/Admin-Package/components/Upload-Form/Upload-Package-Form.comp.js
+++ src/pages/Admin-Package/components/Upload-Form/Upload-Package-Form.comp.js
@@ -3,12 +3,17 @@ import styles from "./Upload-Package-Form.module.css";
 export const UploadPackageForm = () => {
 
     return (
+     
         <div>
             <form className={styles['Upload-Package-Form']}>
                 <div className={styles['Upload-Package-Form-Container']}>
                     <label htmlFor="package_name"> Package name: </label>
-                    <input id="package_name" type="text" placeholder="Package Name"></input>
-                    <p><input type="file" ></input></p>
+                    <input className={styles['Package-Name-Input']} id="package_name" type="text" placeholder="Package Name"></input>
+                    <label htmlFor="description"> Description: </label>
+                    <textarea id="description" className={styles['Description']} cols={1} rows={3}></textarea>
+                    <label htmlFor="file"> Choose a file: </label>
+                    <p><input id="file" type="file" ></input></p>
+                    <hr></hr>
                     <button className={styles['Form-Btn']} type="submit">Upload</button>
                 </div>
 
