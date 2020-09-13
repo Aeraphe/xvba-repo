eaa599d2b3a16b685132145f8442161763dd4e72
aeraph <alberto.aeraph@gmail.com>
2020-09-12 22:01:14 -0300

    Fix css and remove one column

--- src/pages/Admin-Package/components/User-List-Packages/User-List-Packages.comp.js
+++ src/pages/Admin-Package/components/User-List-Packages/User-List-Packages.comp.js
@@ -3,6 +3,7 @@ import styles from './User-List-Packages.module.css'
 import { PackageItemMenuComp } from "../Package-Item-Menu/Package-Item-Menu.comp";
 import PackagesHttpService from "../../../../shared/services/packagesHttp.service";
 
+
 export const UserListPackagesComp = () => {
     const [packages, setPackages] = useState();
 
@@ -26,14 +27,11 @@ export const UserListPackagesComp = () => {
                     <div>Version</div>
                     <div>Stars</div>
                     <div>Installs</div>
-                    <div>Options</div>
+                    
                 </div>
-
             </div>
-
-            <div>
-                {packages}
-            </div>
+            <div>{packages}</div>
+        
         </div>
 
     )
@@ -45,25 +43,28 @@ const handleGetUserPackages = async () => {
     let components = [];
     if (data) {
         data.forEach((element, index) => {
-            components.push(<PackageListItem data={element} item={index+1} key={index}></PackageListItem>)
+            components.push(<PackageListItem data={element} item={index + 1} key={index}></PackageListItem>)
         });
     }
     return components;
 }
 
 const PackageListItem = (props) => {
-    const { name,rating,version,downloads } = props.data
+    const { name, rating, version, downloads, id } = props.data
     return (
-        
-        
-        <div   className={styles['Body-Container']}>
-            <div className={styles['Body-Item']}>
-                <div>{props.item}</div>
-                <div><PackageItemMenuComp >{name}</PackageItemMenuComp></div>
-                <div>{version}</div>
-                <div>{rating}</div>
-                <div>{downloads}</div>
-                <div>Options</div>
+
+
+        <div className={styles['Body-Container']}>
+            <div className={styles['Body-Content']}>
+                <div className={styles['Body-Item']}>{props.item}</div>
+
+                <div className={styles['Body-Item']}>
+                    <PackageItemMenuComp id={id} >{name}</PackageItemMenuComp>
+                </div>
+
+                <div className={styles['Body-Item']}>{version}</div>
+                <div className={styles['Body-Item']}>{rating}</div>
+                <div className={styles['Body-Item']}>{downloads}</div>
             </div>
         </div>
     )
