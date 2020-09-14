c6f88f26720e7dc7b0ad62dffa86183eac096399
aeraph <alberto.aeraph@gmail.com>
2020-09-13 21:58:59 -0300

    Add header link guard

--- src/Template/components/Header.js
+++ src/Template/components/Header.js
@@ -5,22 +5,47 @@ import { Link } from 'react-router-dom';
 let isLogged;
 
 export const Header = () => {
+
     isLogged = useSelector(state => state.auth.isLogged);
+
     return (
         <div className={styles['Container']} >
             <header className={styles.header}>
                 <a href="/"><span style={{ color: '#81B441', fontWeight: 'bold' }}>X</span>vba<span style={{ color: '#6B913A' }}>repository</span></a>
                 <nav >
                     <ul className={styles['header-menu']}>
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/donate">Donate</Link></li>
-                        <li><Link to="/tutorial">Tutorial</Link></li>
-                        <li> <Link to="/upload-package">Publish</Link></li>
-                        <li><Link to="login">{isLogged ? 'Logout' : 'Login'}</Link></li>
+                        {HeaderLinkGuard()}
                     </ul>
                 </nav>
             </header>
         </div>
 
     )
+}
+
+
+const LINKS =
+    [
+        <li><Link to="/">Home</Link></li>,
+        <li><Link to="/donate">Donate</Link></li>,
+        <li><Link to="/tutorial">Tutorial</Link></li>,
+        <li protected="true" > <Link to="/upload-package">Publish</Link></li>,
+        <li><Link to="login">{isLogged ? 'Logout' : 'Login'}</Link></li>
+    ];
+
+
+const HeaderLinkGuard = () => {
+    let linksChecked = LINKS.reduce((prev, next, index) => {
+        if (isLogged) {
+            prev.push({ ...next, key: index })
+        } else if (next.props.protected === undefined) {
+            prev.push({ ...next, key: index })
+        }
+        return prev
+    }, [])
+
+
+
+
+    return (linksChecked)
 }
