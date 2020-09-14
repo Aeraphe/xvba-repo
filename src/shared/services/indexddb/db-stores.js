f26414d78040b78ae4930294e36a3f1fba415117
aeraph <alberto.aeraph@gmail.com>
2020-09-14 17:01:20 -0300

    Create search objectStore

--- src/shared/services/indexddb/db-stores.js
+++ src/shared/services/indexddb/db-stores.js
@@ -2,5 +2,5 @@ export const createStorePackages = (db) => {
     const store = db.createObjectStore('packages', { keyPath: "id" });
     store.createIndex('by_id', 'id', { unique: true })
     store.createIndex('by_name', 'name', { unique: true })
-    console.log('db onupgradeneeded')
+    db.createObjectStore('search', { autoIncrement : true });
 }
