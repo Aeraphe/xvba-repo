b678d88eb2c32ee879041dc8d36106e7443dea93
aeraph <alberto.aeraph@gmail.com>
2020-09-16 21:33:51 -0300

    Add SearchSharedComp on Show-Package Page

--- src/pages/Show-Package/Show-Package.page.js
+++ src/pages/Show-Package/Show-Package.page.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import styles from './Show-Package.module.css'
-
+import { SearchShareComp } from "../../shared/components/Search/Search-comp-shared";
 export const ShowPackagePage = (props) => {
 
     console.log(props);
     return (
 
-        <div className={styles['Show-Package']}>
-            <section>
-                Readme section
-            </section>
-            <section>
+        <div className={styles['Container']}>
+            <div className={styles['Search']}>
+                <SearchShareComp></SearchShareComp>
+            </div>
+            <div className={styles['Content']}>
+            <div className={styles['Logo']}>
+                Readme div
+            </div>
+            <div className={styles}>
                 Package download info
-            </section>
+            </div>
+            </div>
+          
         </div>
     )
 }
+
+
