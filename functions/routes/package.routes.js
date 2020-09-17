108f80a4cfff18f1f197c8b7daeeace9b45f7d29
aeraph <alberto.aeraph@gmail.com>
2020-09-17 18:56:56 -0300

    Update  dowload package route with stream file

--- functions/routes/package.routes.js
+++ functions/routes/package.routes.js
@@ -12,14 +12,37 @@ router
     .post('/', authRoute, async (req, res) => {
         const response = await packageController.addPackage(req)
         res.json(response);
-        
+
     }).get('/', async (req, res) => {
         const response = await packageController.getPackage(req);
         res.json(response);
 
     }).get('/download/:name', async (req, res) => {
         const response = await packageController.getPackageFileForDownload(req);
-        res.json(response);
+      try {
+        if (response.result.meta.code === 200) {
+            const stream = response.stream;
+            res.writeHead(200, { 'Content-Type': 'application/octet-stream','Content-Disposition': 'attachment; ; filename=package.xvba' });
+            stream.on('data', function (data) {
+                res.write(data);
+            });
+
+            stream.on('error', function (err) {
+                console.log('error reading stream', err);
+            });
+
+            stream.on('end', function () {
+              
+                res.end();
+            });
+        } else {
+            res.status(response.meta.code).json(response);
+        }
+      } catch (error) {
+        res.send(error)
+      }
+      
+
 
     }).get('/user-auth', authRoute, async (req, res) => {
         const response = await packageController.getUserAuthPackages(req);
