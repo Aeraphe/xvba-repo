58d37ec13fef995b5feb120ad3671802f0c75733
aeraph <alberto.aeraph@gmail.com>
2020-09-17 00:28:04 -0300

    Fix css

--- src/pages/Show-Package/Show-Package.page.js
+++ src/pages/Show-Package/Show-Package.page.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import styles from './Show-Package.module.css'
 import { SearchShareComp } from "../../shared/components/Search/Search-comp-shared";
-export const ShowPackagePage = (props) => {
+import marked from 'marked';
+import DOMPurify from 'dompurify';
+import { CopyToClipboard } from "../../shared/components/CopyToClipboard/CopyToClipboard-shared";
 
+
+export const ShowPackagePage = (props) => {
+    const mark = { __html: DOMPurify.sanitize(marked('# Markdown')) };
     console.log(props);
     return (
 
@@ -17,12 +22,10 @@ export const ShowPackagePage = (props) => {
                         <div className={styles['Package-Version']}>1.1.2  Public  Published 2 months ago</div>
                     </div>
                     <div className={styles['Package-Info']}>
-
-                        <div className={styles['Package-Info-Readme']}>Readme</div>
+                        <div className={styles['Package-Info-Readme']} dangerouslySetInnerHTML={mark} ></div>
                         <div className={styles['Package-Info-Statistics']}>
-                            <div>
-                                <div className={styles['Install-Title']}>Install</div>
-                            </div>
+                            <div className={styles['Install-Title']}>Install</div>
+                            <p><CopyToClipboard package={'test'} /></p>
                         </div>
                     </div>
                 </div>
