f370d06e45a4e15d47ca51cc9ecd08b1db1c4488
aeraph <alberto.aeraph@gmail.com>
2020-09-23 00:57:12 -0300

    Add getPackageReadme on load component

--- src/pages/Show-Package/Show-Package.page.js
+++ src/pages/Show-Package/Show-Package.page.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Show-Package.module.css'
 import { SearchShareComp } from "../../shared/components/Search/Search-comp-shared";
 import marked from 'marked';
 import DOMPurify from 'dompurify';
 import { CopyToClipboard } from "../../shared/components/CopyToClipboard/CopyToClipboard-shared";
+import PackageHttpService from '../../shared/services/packagesHttp.service';
 
 
 export const ShowPackagePage = (props) => {
-    const mark = { __html: DOMPurify.sanitize(marked('# Markdown')) };
+
     console.log(props);
+    const [readme, setReadme] = useState({ __html: "<p>Loading...</p>" });
+    useEffect(() => {
+        (async () => {
+            const data = await PackageHttpService.getPackageReadme(props.match.params.package);
+            setReadme({ __html: DOMPurify.sanitize(marked(data)) })
+        })()
+    }, [props.match.params.package])
     return (
 
         <div className={styles['Container']}>
@@ -22,7 +30,7 @@ export const ShowPackagePage = (props) => {
                         <div className={styles['Package-Version']}>1.1.2  Public  Published 2 months ago</div>
                     </div>
                     <div className={styles['Package-Info']}>
-                        <div className={styles['Package-Info-Readme']} dangerouslySetInnerHTML={mark} ></div>
+                        <div className={styles['Package-Info-Readme']} dangerouslySetInnerHTML={readme} ></div>
                         <div className={styles['Package-Info-Statistics']}>
                             <div className={styles['Install-Title']}>Install</div>
                             <p><CopyToClipboard package={'test'} /></p>
