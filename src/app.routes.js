992adf55604eca1e92797a18c73635e1c13ab3fb
aeraph <alberto.aeraph@gmail.com>
2020-09-25 19:05:17 -0300

    Create Route for terms and privacy

--- src/app.routes.js
+++ src/app.routes.js
@@ -3,6 +3,9 @@ import { SearchPage } from './pages/Search/Search.page'
 import { LoginPage } from './pages/Login/Login.page';
 import { ShowPackagePage } from "./pages/Show-Package/Show-Package.page";
 import { AdminPackagePage } from './pages/Admin-Package/Admin-Package.page';
+import {PrivacyPolicyPage} from './pages/Privacy-Policy/Privacy-Policy.page'
+import {TermsPage} from './pages/Terms/Terms.page';
+
 import { Route } from 'react-router-dom';
 
 
@@ -14,6 +17,8 @@ export const routes = {
         <Route key="r1" path='/' exact component={SearchPage}></Route>,
         <Route key="r2" path='/login' exact component={LoginPage}></Route>,
         <Route key="r3" path='/show-package/:package' exact component={ShowPackagePage} ></Route>,
+        <Route key="r4" path='/privacy' exact component={PrivacyPolicyPage} ></Route>,
+        <Route key="r5" path='/terms' exact component={TermsPage} ></Route>,
     ]
 }
 
