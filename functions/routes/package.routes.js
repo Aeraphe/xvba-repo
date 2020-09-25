a22d44ec26ee62a4eb59f9a5847e5935a0a860ea
aeraph <alberto.aeraph@gmail.com>
2020-09-24 21:23:17 -0300

    Add status code on allhttp response

--- functions/routes/package.routes.js
+++ functions/routes/package.routes.js
@@ -3,6 +3,7 @@
  * @version v1 
  */
 const express = require('express');
+const { toInteger } = require('lodash');
 const packageController = require('../controller/package.controller');
 const router = express.Router();
 const authRoute = require('../services/middleware/auth_route')
@@ -11,35 +12,39 @@ const authRoute = require('../services/middleware/auth_route')
 router
     .post('/', authRoute, async (req, res) => {
         const response = await packageController.addPackage(req)
-        res.json(response);
+        res.status(toInteger(response.meta.code)).json(response);
 
     }).put('/update', authRoute, async (req, res) => {
         const response = await packageController.addNewPackageVersion(req)
-        res.json(response);
+        res.status(toInteger(response.meta.code)).json(response)
 
     }).get('/', async (req, res) => {
         const response = await packageController.getPackage(req);
-        res.json(response);
+        res.status(toInteger(response.meta.code)).json(response);
+
     }).get('/download/:name', async (req, res) => {
         const data = await packageController.getPackageFileForDownload(req);
         handleDownloadFileStream(res, data)
+
     }).get('/readme/:name', async (req, res) => {
         const data = await packageController.getPackageReadme(req);
         handleTextFileStream(res, data)
+
     }).get('/user-auth', authRoute, async (req, res) => {
         const response = await packageController.getUserAuthPackages(req);
-        res.json(response);
+        res.status(toInteger(response.meta.code)).json(response);
 
     }).post('/fuse-search', async (req, res) => {
         const response = await packageController.fuseSearchPackages(req);
-        res.json(response);
+        res.status(toInteger(response.meta.code)).json(response);
+        
     }).post('/search-name', async (req, res) => {
         const response = await packageController.searchPackageName(req);
-        res.json(response, response.meta.code);
+        res.status(toInteger(response.meta.code)).json(response, response.meta.code);
 
     }).delete('/:id', authRoute, async (req, res) => {
         const response = await packageController.deletePackage(req);
-        res.json(response, response.meta.code);
+        res.status(toInteger(response.meta.code)).json(response, response.meta.code);
 
     })
 
