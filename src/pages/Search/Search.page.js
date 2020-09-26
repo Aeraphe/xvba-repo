c298d718ffd6a74dc0938e53fd2eb7530bbd1c3c
aeraph <alberto.aeraph@gmail.com>
2020-09-25 23:16:05 -0300

    Add SubPage InfoPage on SearchPage

--- src/pages/Search/Search.page.js
+++ src/pages/Search/Search.page.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import styles from "./Search-page.module.css";
 import { SearchComp } from './components/Search/Search.comp';
-
+import { InfoPage } from './sub-pages/Info/Info.page'
 
 export const SearchPage = () => {
 
 
 
     return (
-        <div style={{ marginBottom: '57px' }}>
+        <div className={styles['Container']} >
             <SearchComp></SearchComp>
+            <InfoPage></InfoPage>
         </div>
     )
 }
