8ff409b1ce621592ba24e92f7d0f86633d74b75f
aeraph <alberto.aeraph@gmail.com>
2020-09-26 20:40:01 -0300

    Create Vscode Extension page

---
+++ src/pages/Vscode-Extension/Vscode-Extension-Page.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import styles from './Vscode-Extension-Page.module.css'
+import marked from 'marked';
+import DOMPurify from 'dompurify';
+
+import { readme } from './extension-readme';
+
+export const VscodeExtensionPage = () => {
+
+    const readmeExtension = { __html: DOMPurify.sanitize(marked(readme())) }
+    return (
+        <>
+            <div className={styles['Container']}>
+                <div className={styles['Container-Grid']}>
+                    <div className={styles['Menu-Container']}>
+                        <div className={styles['Menu-Title']}>Menu</div>
+                        <div className={styles['Menu']}>
+                            <ul>
+                                <li className={styles['Menu-Btn']}><span>: :</span> About Xvba Extension</li>                               
+                            </ul>
+                        </div>
+                    </div>
+                    <div className={styles['Readme-Container']}>
+                        <div className={styles['Readme']} dangerouslySetInnerHTML={readmeExtension} ></div>
+                    </div>
+
+                </div>
+
+            </div>
+        </>
+    )
+}
