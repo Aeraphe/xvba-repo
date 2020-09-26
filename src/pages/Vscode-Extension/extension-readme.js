f6789f661927fbe032e72ab057fd63bedb97a107
aeraph <alberto.aeraph@gmail.com>
2020-09-26 20:40:43 -0300

    Add readme files for xvba cli and extension

---
+++ src/pages/Vscode-Extension/extension-readme.js
@@ -0,0 +1,53 @@
+export const readme = () => {
+  return (`
+# XVBA Extension for VSCode
+
+<p>
+This <b>free</b> extension provides a way to work in VSCode with VBA files (import/export) from Excel with Live Server.
+<br/>Combined with <b>XVBA-Cli</b> and <b>Git</b> you can create more powerful and professional Excel apps with code management and work in Teams.
+
+The list below is some off new features vba language will have with XVBA
+
+- 1 Import excel VBA code to files (With error log)
+- 2 Namespace (Organize files in folders)
+- 3 Live Server (With error log)
+- 4 Run Macros from VSCode TreeView (With error log)
+- 5 Auto Complete (Language Serve, in progress)
+- 6 Syntax Highlighting
+- 7 Snippets
+- 8 Auto Indentation Rules
+- 9 Create Your own packages with Xvba-cli and share in XVBA Repository
+- 10 Don't try to recreate the wheel, search for packages in xvba.dev and install on yous project
+- 11 Track code changes and work in <b>Teams</b> with Git
+- 12 Stop the Live server and load VBA files to Excel Manually
+- 13 XVBA-CLI Command Line Interface for XVBA VSCode extension <a href="https://www.npmjs.com/package/@localsmart/xvba-cli"> <b>-->> @localsmart/xvba-cli</b></a>
+
+For <b>more details</b> see the extension on VSCode Market Place: <a href="https://marketplace.visualstudio.com/items?itemName=local-smart.excel-live-server"/><b>-->> XVBA Extension</b></a>
+
+## We want to hear from you so, all feedbacks are welcome.
+- Mail : alberto.aeraph@gmail.com
+- WhatsApp: +55 31 9 9229 5626
+- Github : aeraphe
+
+## Support XVBA 
+
+<p> If you find it useful, please consider supporting it. </p>
+
+- Become a <a href="https://www.patreon.com/aeraphe"> Sponsor </a> - join the growing group of generous backers
+
+Also <b>please</b> write a review, star me on <b>GitHub</b>, and follow me on github.<br/>
+My desirer and my passion is to create a useful tool for many people it's possible
+<br/><br/><b>Thanks to every one that are using XVBA</b>
+<br/> More and more features will came..
+
+## Youtube (Under constrution)
+ 
+ - How to install and why XVBA <a>(Let's Goo)</a>
+ - Start new Excel VBA Project in VSCode with XVBA (Let's Goo)
+ - Install XVBA-CLI and add third package to your project (Let's Goo)
+ - Create your own VBA package and share in github with XVBA  (Let's Goo)
+
+
+
+`)
+}
---
+++ src/pages/Xvba-Cli/xvba-cli-readme.js
@@ -0,0 +1,41 @@
+export const readme = () => {
+  return (`
+# XVBA-Cli <br/>It's Command Line Interface for XVBA Extension
+### Make your code reusable for every one
+<p>
+This <b>free</b> app provides a way to create, install, delete, and share VBA packages.
+<br/> So, don't try to recreate the wheel, search for packages in xvba.dev and install on your project .</p>
+
+
+
+The list below is some off new features vba language will have with XVBA
+
+- 1 Create vba packages with version control 
+- 2 Build and Share your packages in www.xvba.dev
+- 3 Work in Teams 
+- 4 Make your code reusable
+- 5 Create vba files (bas os cls) with command 
+
+
+For <b>more details</b> see the nodejs app  on NPM : <a href="https://www.npmjs.com/package/@localsmart/xvba-cli"/><b>-->> XVBA Cli</b></a>
+
+## We want to hear from you so, all feedbacks are welcome.
+- Mail : alberto.aeraph@gmail.com
+- WhatsApp: +55 31 9 9229 5626
+- Github : aeraphe
+
+## Support XVBA 
+
+<p> If you find it useful, please consider supporting it. </p>
+
+- Become a <a href="https://www.patreon.com/aeraphe"> Sponsor </a> - join the growing group of generous backers
+
+Also <b>please</b> write a review, star me on <b>GitHub</b>, and follow me on github.<br/>
+My desirer and my passion is to create a useful tool for many people it's possible
+<br/><br/><b>Thanks to every one that are using XVBA</b>
+<br/> More and more features will came..
+
+
+
+`)
+}
