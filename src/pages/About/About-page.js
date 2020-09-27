ba4ff8605328b65f268a3b23d30cca9ea5113563
aeraph <alberto.aeraph@gmail.com>
2020-09-27 16:45:58 -0300

    Create About Page

---
+++ src/pages/About/About-page.js
@@ -0,0 +1,32 @@
+import React from "react";
+import styles from './About-page.module.css'
+
+
+export const AboutPage = () => {
+    return (
+        <div className={styles['Container']}>
+            <div className={styles['Content']}>
+                <h1> About XVBA</h1>
+                <p> Xvba Repository is under construction version "b"</p>
+                <p>Some features may not working or fail</p>
+                <p>You can test and create your packages with Xvba-cli</p>
+                <h4>Some example packages are:</h4>
+                <ul>
+                    <li>linear</li>
+                    <li>sql</li>
+                    <li>gauss</li>
+                </ul>
+               
+
+                <h3>Support XVBA </h3>
+                <p> If you find it useful, please consider supporting it. </p>
+                <p>Become a <a href="https://www.patreon.com/aeraphe"> Sponsor </a> - join the growing group of generous backers</p>
+
+Also <b>please</b> write a review, star me on <b>GitHub</b>, and follow me on github.<br />
+My desirer and my passion is to create a useful tool for many people it's possible
+<br /><br /><b>Thanks to every one that are using XVBA</b>
+                <br /> More and more features will came..
+            </div>
+        </div>
+    )
+}
